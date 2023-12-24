bbda4f4fdfa73ac138512931f1fd4e9efede42b3
accela673 <sardar.kasmaliev@alatoo.edu.kg>
2023-12-24 17:08:25 +0600

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -9,24 +9,30 @@ const timeout = require('connect-timeout');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Middleware для разрешения CORS
   app.enableCors({
-    origin: ['http://10.129.1.98:3000', 'http://localhost:3000'],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
+    allowedHeaders: 'Content-Type,Authorization',
   });
+
   const PORT = process.env.PORT || 8080;
   app.setGlobalPrefix('api');
   app.use(timeout('50s'));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   const config = new DocumentBuilder()
     .setTitle("Scientist's room")
     .setDescription('API documentation for project at Ala-Too ')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
-  app.enableCors();
+
   await app.listen(PORT, () => console.log(`App started on port ${PORT}`));
 }
 bootstrap();

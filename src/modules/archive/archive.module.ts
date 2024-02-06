d9f4b5b7df4b9de16ec7162a52557ce64529e645
accela673 <sardar.kasmaliev@alatoo.edu.kg>
2024-02-06 14:27:24 +0600

    fixed

--- src/modules/archive/archive.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ArchiveService } from './archive.service';
-import { ArchiveController } from './archive.controller';
-
-@Module({
-  controllers: [ArchiveController],
-  providers: [ArchiveService]
-})
-export class ArchiveModule {}

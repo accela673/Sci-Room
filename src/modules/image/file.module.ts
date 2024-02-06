3dd39fa3c281a775b42b556a237c4e148562909c
accela673 <sardar.kasmaliev@alatoo.edu.kg>
2024-02-06 14:25:58 +0600

    Changed approve and decline

--- src/modules/image/file.module.ts
+++ src/modules/image/file.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { FileService } from './file.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Image } from './entities/image.entity';
-import { CloudinaryModule } from '../../services/cloudinary/cloudinary.modules';
+import { CloudinaryModule } from '../cloudinary/cloudinary.modules';
 import { FileController } from './file.controller';
 import { Pdf } from './entities/pdf.entity';
 import { DocX } from './entities/txt.entity';
